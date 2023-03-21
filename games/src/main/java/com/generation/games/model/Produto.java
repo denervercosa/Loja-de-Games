e563ef53015ee81a90c00798563bbb70d3af28db
denervercosa <dener.vercosa@gmail.com>
2023-03-21 13:53:54 -0300

    Crowd Loja de jogos digitais e fisicos

---
+++ games/src/main/java/com/generation/games/model/Produto.java
@@ -0,0 +1,85 @@
+package com.generation.games.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 2, max = 50 , message = "O atributo nome é obrigatório")
+	private String nome;
+	
+	
+	@NotBlank
+	@Size(min = 2, max = 50 , message = "O atributo nome é obrigatório")
+	private String genero;
+	
+	@NotNull(message = "O atributo preço é obrigatório")
+	@Size(min = 2, max = 50 , message = "O atributo nome é obrigatório")
+	private BigDecimal preco;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+
+}
