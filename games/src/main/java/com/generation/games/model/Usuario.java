154e0aab43fa53c5466bef99459bc2f6c6b4792d
denervercosa <dener.vercosa@gmail.com>
2023-03-27 18:41:14 -0300

    Impleta Security e Service na API Loja Games

---
+++ games/src/main/java/com/generation/games/model/Usuario.java
@@ -0,0 +1,66 @@
+package com.generation.games.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Nome é obrigatório")
+	private String nome;
+	
+	@NotBlank(message = "O atributo Usuário é obrigatório")
+	@Email(message = "O atributo Usuário deve ser um email válido")
+	private String usuario;
+	
+	@NotBlank(message = "O atributo Senha é obrigatório")
+	@Size(min = 8, message = "A senha ter ter no minimo 8 caracteres")
+	private String senha;
+	
+	@Size(max = 5000, message = "O link da foto não deve ter mais que 5000 caracteres")
+	private String foto;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+}
